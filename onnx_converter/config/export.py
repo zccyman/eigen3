846cbe9b82bd8ff8562c5376d27e3eca4b001ffc
zccyman <zccyman@163.com>
2023-12-18 01:20:20 +0000

    fix export;

--- onnx_converter/config/export.py
+++ onnx_converter/config/export.py
@@ -9,6 +9,7 @@
 secret_key = "henson.zhang@timesintelli.com"
 
 is_debug = False
+is_voice_model = False
 export_mode_c = True
 chip_type = "AT1K"  # [AT1K, AT5050,]
 
@@ -28,6 +29,7 @@ bits = dict(
     DATA_C_EXTEND=AT5050_C_EXTEND,
     chip_type=chip_type,
     bgr_format=True,
+    is_voice_model=is_voice_model,    
     is_debug=is_debug,
 )
 
--- onnx_converter/config/export_v2.py
+++ onnx_converter/config/export_v2.py
@@ -8,6 +8,7 @@
 
 
 is_debug = False
+is_voice_model = False
 export_mode_c = True
 chip_type = "AT1K"  # [AT1K, AT5050,]
 
@@ -27,6 +28,7 @@ bits = dict(
     DATA_C_EXTEND=AT5050_C_EXTEND,
     chip_type=chip_type,
     bgr_format=True,
+    is_voice_model=is_voice_model,    
     is_debug=is_debug,
 )
 
